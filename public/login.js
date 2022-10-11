830bb4daebec309e370fa489ee149e823c58e5d8
eungyolee <daaay0123@naver.com>
2022-10-11 18:07:37 +0900

    로그인 시스템 개편

--- public/login.js
+++ public/login.js
@@ -1,45 +1,50 @@
 const db = firebase.firestore();
+var provider = new firebase.auth.GoogleAuthProvider();
+
+function getCookie(name) {
+  var value = document.cookie.match("(^|;) ?" + name + "=([^;]*)(;|$)");
+  return value ? value[2] : null;
+}
 
 $("#register").click(function () {
-  var email = $("#email-new").val();
-  var password = $("#pw-new").val();
-
   firebase
     .auth()
-    .signInWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-      // Signed in
-      var user = userCredential.user;
-      document.cookie = `user=${user.uid}`;
+    .signInWithPopup(provider)
+    .then((result) => {
+      /** @type {firebase.auth.OAuthCredential} */
+      var credential = result.credential;
+
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      console.log(user);
+      document.cookie = `uuid=${user.uid}`;
+      document.cookie = `username=${user.displayName}`;
+      document.cookie = `email=${user.email}`;
       alert("로그인에 성공하였습니다.");
-      window.location.href = "/";
+      window.location = "/";
+      // ...
     })
     .catch((error) => {
+      alert("오류가 발생했습니다. 다시 시도해주세요.");
+      // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
-      console.log(errorMessage);
-      if (
-        errorMessage ===
-        "Firebase: The email address is badly formatted. (auth/invalid-email)."
-      ) {
-        alert("이메일 형식을 입력하여 주십시오.");
-      } else if (
-        errorMessage ===
-        "Firebase: There is no user record corresponding to this identifier. The user may have been deleted. (auth/user-not-found)."
-      ) {
-        alert("존재하지 않는 계정입니다.");
-      } else if (
-        errorMessage ===
-        "Firebase: The password is invalid or the user does not have a password. (auth/wrong-password)."
-      ) {
-        alert("잘못된 비밀번호입니다.");
-      }
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      console.log(error);
     });
 });
 
-if (document.location.href === "https://yeomgyeong-wiki.web.app/write.html") {
-  var [cookieValue] = document.cookie.split("=", 1);
-  if (cookieValue === "user") {
+if (
+  document.location.href == "https://ymgng-wiki.eungyolee.studio/write.html"
+) {
+  var [cookieValue] = getCookie("uuid");
+  console.log(cookieValue);
+  if (cookieValue !== null) {
     const db = firebase.firestore();
 
     $("#send").click(function () {
@@ -47,12 +52,16 @@ if (document.location.href === "https://yeomgyeong-wiki.web.app/write.html") {
       var year = date.getFullYear();
       var month = date.getMonth() + 1;
       var day = date.getDate();
-      var user = document.cookie.substr(5);
+      var userid = getCookie("uuid");
+      var useremail = getCookie("email");
+      var username = getCookie("username");
       var data = {
         제목: $("#title").val(),
         내용: $("#content").val(),
         날짜: `${year}.${month}.${day}`,
-        작성자: user,
+        작성자_아이디: userid,
+        작성자_이메일: useremail,
+        작성자: username,
       };
       db.collection("wiki")
         .add(data)
